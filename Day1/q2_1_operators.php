09389d6150385912fb6be401a38036da583c123c
MUSALE <musalenidsa18it@student.mes.ac.in>
2020-10-01 13:25:45 +0530

    First commit

---
+++ Day1/q2_1_operators.php
@@ -0,0 +1,15 @@
+<?php
+$num1=6;
+$num2=2;
+$a=$num1+$num2;
+$s=$num1-$num2;
+$m=$num1*$num2;
+$d=$num1/$num2;
+echo "addition is $a <br>";
+
+echo "subtraction is $s <br>";
+
+echo "Multiplication is $m <br>";
+
+echo " Division is $d";
+?>
